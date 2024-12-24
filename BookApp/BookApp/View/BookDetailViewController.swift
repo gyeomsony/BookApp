97504ef4e453774e98f374db4f68ac95e7ecdcde
sonny <gyeomsony@gmail.com>
2024-12-25 00:01:46 +0900

    ✨[Feat] 더보기 버튼 추가

--- BookApp/BookApp/View/BookDetailViewController.swift
+++ BookApp/BookApp/View/BookDetailViewController.swift
@@ -47,13 +47,13 @@ class BookDetailViewController: UIViewController {
     
     private let descriptionLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.numberOfLines = 7
         label.text = """
 어느 날 갑자기 출현한 정체불명의 식인종 거인들에 의해 인류의 태반이 잡아 먹히며 인류는 절멸 위기에 처한다.
 목숨을 부지한 생존자들은 높이 50m의 거대한 삼중의 방벽 월 마리아, 월 로제, 월 시나를 건설하여 그 곳으로 도피, 방벽 내부에서 100여 년에 걸쳐 평화의 시대를 영위하게 된다.
 그리고 100여 년이 지난 845년, 대부분 주민들이 오래도록 지속되어 온 평화에 안주하는 반면, 주인공 엘런 예거는 사람들이 거인들에게 둘러싸여 벽 안에서 가축같이 살아가는 세계에 커다란 불만을 느낀다. 그는 벽 밖의 세계로 나가서 세계를 자유롭게 누비며 탐험하는 것을 열망한다. 
 """
-        label.font = .systemFont(ofSize: 15)
+        label.font = .systemFont(ofSize: 13)
         label.textColor = .black
         label.lineBreakMode = .byTruncatingTail
         label.textAlignment = .justified
@@ -69,6 +69,16 @@ class BookDetailViewController: UIViewController {
         button.backgroundColor = .systemBlue
         return button
     }()
+    // 더보기
+    private let moreButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("더보기", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        return button
+    }()
+    
+    private var isExpanded = false // 텍스트 확장 되었는지 확인
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,6 +94,7 @@ class BookDetailViewController: UIViewController {
             bookImageView,
             priceLabel,
             descriptionLabel,
+            moreButton,
             addButton
         ].forEach { view.addSubview($0) }
         
@@ -114,6 +125,11 @@ class BookDetailViewController: UIViewController {
             $0.leading.trailing.equalToSuperview().inset(10)
         }
         
+        moreButton.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+        
         addButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(40)
             $0.centerX.equalToSuperview()
@@ -124,10 +140,30 @@ class BookDetailViewController: UIViewController {
     
     // 액션 설정
     private func setupActions() {
-        let action = UIAction { _ in
-            self.addBook()
+        let moreAction = UIAction { _ in
+            self.toggleDescription()
+        }
+        moreButton.addAction(moreAction, for: .touchUpInside)
+        
+        let addAction = UIAction { _ in
+            self.addBook() // 장바구니에 책 담기
+        }
+        addButton.addAction(addAction, for: .touchUpInside)
+    }
+    
+    @objc private func toggleDescription() {
+        isExpanded.toggle()
+
+        // 텍스트가 확장되면, 전체 텍스트를 보여주고 "더보기" 버튼 텍스트를 변경
+        if isExpanded {
+            descriptionLabel.numberOfLines = 0
+            moreButton.setTitle("닫기", for: .normal)
+        } else {
+            descriptionLabel.numberOfLines = 7
+            moreButton.setTitle("더보기", for: .normal)
         }
     }
+    
     // 우선 그냥 해놓기
     @objc private func addBook() {
         print("책이 담겼습니다.")
