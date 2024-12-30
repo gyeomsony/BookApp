bae6eee0e4cc898a600346e00b59fd36184cc9c4
sonny <gyeomsony@gmail.com>
2024-12-30 09:43:33 +0900

    ✨[Feat] APIManager 클래스에 api키 넣기

--- BookApp/BookApp/JSONDecoderManager.swift
+++ BookApp/BookApp/JSONDecoderManager.swift
@@ -27,3 +27,49 @@ class JSONDecoderManager {
     }
 }
 
+class APIManager {
+    static let shared = APIManager()
+    private let decoderManager = JSONDecoderManager.shared
+    
+    private init() {}
+    
+    func fetchBooks(query: String) -> Single<[KakaoBook]> {
+        let apiKey = "3a4facecbe99c3bf5eab0a5480368bd5"
+        let urlString = "https://dapi.kakao.com/v3/search/book?query=\(query)"
+        
+        guard let url = URL(string: urlString) else {
+            return Single.error(NSError(domain: "Invalid URL", code: -1, userInfo: nil))
+        }
+        
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = ["Authorization": "KakaoAK \(apiKey)"]
+        
+        return Single<Data>.create { single in
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    single(.failure(error))
+                    return
+                }
+                
+                guard let data = data else {
+                    single(.failure(NSError(domain: "No Data", code: -1, userInfo: nil)))
+                    return
+                }
+                
+                single(.success(data))
+            }
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+        .flatMap { data in
+            self.decoderManager.decode(KakaoBookResponse.self, from: data)
+        }
+        .map { response in
+            response.documents
+        }
+    }
+}
+
